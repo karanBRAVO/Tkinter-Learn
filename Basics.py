a9596c961a61ed5a804180adf29072b71f638a91
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-03-01 18:12:51 +0530

    change the title and size of window

--- Basics.py
+++ Basics.py
@@ -4,5 +4,17 @@ from tkinter import ttk
 # create the window
 window = tk.Tk("Screen name", "Base name")
 
+WINDOW_WIDTH = 480
+WINDOW_HEIGHT = 450
+
+# set the window title
+window.title("Tkinter Basics")
+
+# set the width and height of the window
+window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
+
+# set the minimum size of the window
+window.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
+
 # run the loop
 window.mainloop()
